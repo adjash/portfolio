17a1786e51e584e49cfc884fca52194d05eccbf6
Adam <adam.hardie@transiris.com>
2020-05-05 17:53:08 +0100

    small change to script to check if element is > rather than >=

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,8 +11,8 @@ $( document ).ready( () => {
 var isInViewport = function (elem) {
 	var distance = elem.getBoundingClientRect();
 	return (
-		distance.top >= 0 &&
-		distance.left >= 0 &&
+		distance.top > 0 &&
+		distance.left > 0 &&
 		distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
 		distance.right <= (window.innerWidth || document.documentElement.clientWidth)
 	);
